028209011359805fc99d0d08c310909dff97b30e
Pormiiles <andrelima.santos2370@gmail.com>
2024-08-17 23:00:45 -0300

    Testando consumo da API com getAllUsers

--- src/main/java/com/example/user/domain/config/SpringClient.java
+++ src/main/java/com/example/user/domain/config/SpringClient.java
@@ -8,9 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import com.example.user.domain.models.LoginDTO;
 import com.example.user.domain.models.User;
-import com.example.user.domain.models.UserCreateDTO;
 
 import reactor.core.publisher.Mono;
 
@@ -28,35 +26,6 @@ public class SpringClient {
                 .build(); // Constrói a instância de WebClient.
     }
 
-    // Método para criar um novo usuário. Envia um POST request com os dados do usuário.
-    public Mono<User> createUser(UserCreateDTO data) {
-        return webClient.post() // Define o método HTTP como POST.
-                .uri("/user") // Define o endpoint da API para criar um usuário.
-                .bodyValue(data) // Envia os dados do DTO no corpo da requisição.
-                .retrieve() // Faz a requisição e espera uma resposta.
-                .bodyToMono(User.class) // Converte o corpo da resposta para um objeto do tipo User.
-                .onErrorMap(throwable -> new RuntimeException("Failed to create user: " + throwable.getMessage())); // Mapeia erros para uma exceção genérica de runtime.
-    }
-
-    // Método para realizar o login. Envia um POST request com os dados de login.
-    public Mono<User> login(LoginDTO data) {
-        return webClient.post() // Define o método HTTP como POST.
-                .uri("/user/login") // Define o endpoint da API para login.
-                .bodyValue(data) // Envia os dados do DTO no corpo da requisição.
-                .retrieve() // Faz a requisição e espera uma resposta.
-                .bodyToMono(User.class) // Converte o corpo da resposta para um objeto do tipo User.
-                .onErrorMap(throwable -> new RuntimeException("Login failed: " + throwable.getMessage())); // Mapeia erros para uma exceção genérica de runtime.
-    }
-
-    // Método para buscar um usuário por ID. Envia um GET request com o ID do usuário como parâmetro.
-    public Mono<User> getUserById(int id) {
-        return webClient.get() // Define o método HTTP como GET.
-                .uri("/user/{id}", id) // Define o endpoint da API para buscar um usuário pelo ID, substituindo {id} pelo valor do parâmetro.
-                .retrieve() // Faz a requisição e espera uma resposta.
-                .bodyToMono(User.class) // Converte o corpo da resposta para um objeto do tipo User.
-                .onErrorMap(throwable -> new RuntimeException("Failed to retrieve user by id: " + throwable.getMessage())); // Mapeia erros para uma exceção genérica de runtime.
-    }
-
     // Método para buscar todos os usuários. Envia um GET request.
     public Mono<List<User>> getAllUsers() {
         return webClient.get() // Define o método HTTP como GET.
@@ -65,15 +34,5 @@ public class SpringClient {
                 .bodyToFlux(User.class) // Converte o corpo da resposta para um Fluxo de objetos do tipo User.
                 .collectList() // Coleta os elementos do Fluxo em uma Lista.
                 .onErrorMap(throwable -> new RuntimeException("Failed to retrieve users: " + throwable.getMessage())); // Mapeia erros para uma exceção genérica de runtime.
-    }
-
-    // Método para deletar um usuário pelo ID. Envia um DELETE request com o ID do usuário como parâmetro.
-    public Mono<Void> deleteUser(int id) {
-        return webClient.delete() // Define o método HTTP como DELETE.
-                .uri("/user/{id}", id) // Define o endpoint da API para deletar um usuário pelo ID, substituindo {id} pelo valor do parâmetro.
-                .retrieve() // Faz a requisição e espera uma resposta.
-                .bodyToMono(Void.class) // Converte o corpo da resposta para um objeto do tipo Void, já que não se espera um corpo na resposta.
-                .onErrorMap(throwable -> new RuntimeException("Failed to delete user: " + throwable.getMessage())) // Mapeia erros para uma exceção genérica de runtime.
-                .then(); // Finaliza a cadeia reativa e retorna um Mono<Void>.
     }
 }
